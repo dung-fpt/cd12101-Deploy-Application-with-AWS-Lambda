06c360dd1692446ba576b7c08b8564399a3cf7cf
dung-fpt <dung.us.nf@gmail.com>
2024-09-28 00:51:55 -0400

    update config.ts

---
+++ starter/client/config.ts
@@ -0,0 +1,7 @@
+const apiId = 'https://dev-p7lj87noc1cmlkju.us.auth0.com/api/v2/'
+export const apiEndpoint = `https://60z30lyzl7.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-p7lj87noc1cmlkju.us.auth0.com',
+  clientId: '9PWJYCf7G76KPM94mwZy0Ea190J9KI3G'
+}
